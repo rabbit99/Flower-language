20257317e97670e611a36e595bc0ae8d2f93eeac
vvbest2012 <vvbest2012@gmail.com>
2020-09-19 19:27:35 +0800

    upload main scene and the ref.

---
+++ Assets/william/Scripts/FireTrap.cs
@@ -0,0 +1,56 @@
+﻿using Gamekit2D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTrap : MonoBehaviour
+{
+    public float time_gate = 2f;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnHitDamageable(Damager origin, Damageable damageable)
+    {
+        //Debug.Log("fire wall is OnHitDamageable " + origin.name + "  " + damageable.name);
+    }
+
+    public void OnHitNonDamageable(Damager origin)
+    {
+        //FindSurface(origin.LastHit);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        NotificationCenter.Default.Post(this, NotificationKeys.OnFire, collision.gameObject.name);
+        //Debug.Log("FireTrap Trigger In, " + collision.gameObject.name);
+        timer = 0;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("FireTrap Trigger Stay, " + other.gameObject.name);
+        timer += Time.deltaTime;
+        if(timer >= time_gate)
+        {
+            NotificationCenter.Default.Post(this, NotificationKeys.OnFire, other.gameObject.name);
+            timer = 0;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        NotificationCenter.Default.Post(this, NotificationKeys.OutOfFire, collision.gameObject.name);
+        Debug.Log("FireTrap Trigger Out, " + collision.gameObject.name);
+        timer = 0;
+    }
+}
---
+++ Assets/william/Scripts/FlowerCanvas.cs
@@ -0,0 +1,77 @@
+﻿using Gamekit2D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FlowerCanvas : MonoBehaviour
+{
+    public static FlowerCanvas Instance { get; protected set; }
+
+    public GameObject QuickKeyPrefab;
+    public Transform QuickKeyTransform;
+    public string[] itemNames;
+    protected const float k_KeyIconAnchorWidth = 0.11f;
+
+    public Sprite[] sprites;
+
+    private List<QuickKey> QuickKeys = new List<QuickKey>();
+    private int setQuickKeyIndex = 0;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetInitialQuickKeyCount();
+    }
+
+
+    public void SetInitialQuickKeyCount()
+    {
+
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject quickKeyPrefab = Instantiate(QuickKeyPrefab);
+            quickKeyPrefab.transform.SetParent(QuickKeyTransform);
+            RectTransform healthIconRect = quickKeyPrefab.transform as RectTransform;
+            QuickKeys.Add(quickKeyPrefab.GetComponent<QuickKey>());
+            //healthIconRect.anchoredPosition = Vector2.zero;
+            //healthIconRect.sizeDelta = Vector2.zero;
+            //healthIconRect.anchorMin -= new Vector2(k_KeyIconAnchorWidth, 0f) * i;
+            //healthIconRect.anchorMax -= new Vector2(k_KeyIconAnchorWidth, 0f) * i;
+            //m_KeyIconAnimators[i] = healthIcon.GetComponent<Animator>();
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeKeyUI(string itemName)
+    {
+        //for (int i = 0; i < keyNames.Length; i++)
+        //{
+        //    m_KeyIconAnimators[i].SetBool(m_HashActivePara, controller.HasItem(keyNames[i]));
+        //}
+
+        //search sprite by item name.
+        foreach(var go in sprites)
+        {
+            if(go.name == itemName)
+            {
+                if(setQuickKeyIndex < QuickKeys.Count)
+                {
+                    QuickKeys[setQuickKeyIndex].btnImage.sprite = go;
+                    setQuickKeyIndex++;
+                }
+            }
+        }
+    }
+}
