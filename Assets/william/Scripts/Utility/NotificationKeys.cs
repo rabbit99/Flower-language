cd127e1077e4620e4986efe61b56579fa5bd8056
vvbest2012 <vvbest2012@gmail.com>
2020-09-19 20:43:46 +0800

    added notification feature.

---
+++ Assets/william/Scripts/Utility/NotificationKeys.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+
+public class NotificationKeys
+{
+	public const string InTheLadder = "InTheLadder";
+    public const string OutTheLadder = "OutTheLadder";
+    public const string InTheTool = "InTheTool";
+    public const string OutTheTool = "OutTheTool";
+
+    public const string InTheFixablePipe = "InTheFixablePipe";
+    public const string OutTheFixablePipe = "OutTheFixablePipe";
+    public const string RepairFinish = "RepairFinish";
+    public const string MonsterHurt = "MonsterHurt";
+    public const string RobotHurt = "RobotHurt";
+    public const string GameStart = "GameStart";
+    public const string GameOver = "GameOver";
+
+    public const string OnFire = "OnFire";
+    public const string OutOfFire = "OutOfFire";
+}
+
+public class ItemLookUp
+{
+    public static Dictionary<string, int> ItemLookUpTable = new Dictionary<string, int>
+    {
+        { "cactus",5 },
+    };
+}
