8d5fe186e1af22bca8d4b9b1279f28ca1691a8bf
vvbest2012 <vvbest2012@gmail.com>
2020-10-01 22:22:21 +0800

    upload the resource of 2DGameKit.

---
+++ Assets/2DGamekit/Scripts/Character/StateMachineBehaviours/Player/ClimbSMB.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Gamekit2D
+{
+    public class ClimbSMB : SceneLinkedSMB<PlayerCharacter>
+    {
+        public override void OnSLStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            //m_MonoBehaviour.TeleportToColliderBottom();
+        }
+
+        public override void OnSLStateNoTransitionUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            //m_MonoBehaviour.UpdateFacing();
+            m_MonoBehaviour.CheckForCrouching();
+            //m_MonoBehaviour.CheckForHoldingGun();
+            m_MonoBehaviour.CheckForGrounded();
+            m_MonoBehaviour.CheckForClimbed();
+            //if (m_MonoBehaviour.CheckForFallInput())
+            //    m_MonoBehaviour.MakePlatformFallthrough();
+            //m_MonoBehaviour.GroundedVerticalMovement();
+            //m_MonoBehaviour.GroundedHorizontalMovement(false);
+        }
+    }
+}
---
+++ Assets/2DGamekit/Scripts/Character/StateMachineBehaviours/Player/CrouchSMB.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Gamekit2D
+{
+    public class CrouchSMB : SceneLinkedSMB<PlayerCharacter>
+    {
+        public override void OnSLStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.TeleportToColliderBottom();
+        }
+
+        public override void OnSLStateNoTransitionUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.UpdateFacing();
+            m_MonoBehaviour.CheckForClimbed();
+            m_MonoBehaviour.CheckForCrouching();
+            m_MonoBehaviour.CheckForHoldingGun();
+            m_MonoBehaviour.CheckForGrounded ();
+            if(m_MonoBehaviour.CheckForFallInput())
+                m_MonoBehaviour.MakePlatformFallthrough ();
+            m_MonoBehaviour.GroundedVerticalMovement ();
+            m_MonoBehaviour.GroundedHorizontalMovement (false);
+            
+        }
+    }
+}
