c52f99fd38e650cbe166bce22c109f1788994e8d
vvbest2012 <vvbest2012@gmail.com>
2020-12-16 23:58:58 +0800

    新增技能冷卻功能

--- Assets/william/Scripts/UI/SkillBtn.cs
+++ Assets/william/Scripts/UI/SkillBtn.cs
@@ -9,21 +9,33 @@ public class SkillBtn : MonoBehaviour
     public GameObject iconObj;
     public Button iconBtn;
     public string SkillName = string.Empty;
+    public float cooldownTime = 0;
+    public Text cooldownText;
+
+    private CooldownTimer CooldownTimer;
     // Start is called before the first frame update
     void Start()
     {
+        CooldownTimer = new CooldownTimer(cooldownTime);
+        CooldownTimer.TimerCompleteEvent += ActiveSkill;
         iconBtn.onClick.AddListener(UseSkill);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (CooldownTimer.IsActive)
+        {
+            CooldownTimer.Update(Time.deltaTime);
+            cooldownText.text = CooldownTimer.TimeRemaining.ToString("0.0");
+        }
+
     }
 
     private void OnDestroy()
     {
         iconBtn.onClick.RemoveListener(UseSkill);
+        CooldownTimer.TimerCompleteEvent -= ActiveSkill;
     }
 
     public void SetSkill(bool active)
@@ -36,5 +48,14 @@ public class SkillBtn : MonoBehaviour
     {
         //
         PlayerCharacter.PlayerInstance.UseSkill(SkillName);
+        iconBtn.interactable = false;
+        CooldownTimer.Start();
+        cooldownText.gameObject.SetActive(true);
+    }
+
+    private void ActiveSkill()
+    {
+        iconBtn.interactable = true;
+        cooldownText.gameObject.SetActive(false);
     }
 }
